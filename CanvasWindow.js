f6cba72fed25ea2d0bb6ac57f6bcb5a5600be5ea
Gerard <gerard@wide-eyes.it>
2016-06-17 14:08:11 +0200

    1408

--- CanvasWindow.js
+++ CanvasWindow.js
@@ -2,61 +2,61 @@ import isMobile from 'ismobilejs'
 
 import EditPoint from './EditPoint'
 
-export default function CanvasWindow(options) {
-  return _CanvasWindow(options);
+export default function CanvasWindow (options) {
+  return _CanvasWindow(options)
 
-  function _CanvasWindow(options) {
-    let origin;
-    let opposite;
-    let prevBbox;
+  function _CanvasWindow (options) {
+    let origin
+    let opposite
+    let prevBbox
 
-    let editPoints = [];
-    let selectedEP = {};
+    let editPoints = []
+    let selectedEP = {}
 
-    let minSize = 60;
-    let delta = 15;
+    let minSize = 60
+    let delta = 15
     if (isMobile.phone) delta = 30
 
-    const globalAlpha = 0.3;
-    const canvas = options.canvas;
-    const ctx = canvas.getContext('2d');
+    const globalAlpha = 0.3
+    const canvas = options.canvas
+    const ctx = canvas.getContext('2d')
 
-    const w = canvas.width;
-    const h = canvas.height;
+    const w = canvas.width
+    const h = canvas.height
 
     // init values
-    let p1 = origin = {x: w / 3, y: h / 3}; // upper left corner of a bbox
-    let p2 = opposite = {x: 2 * w / 3, y: 2 * h / 3}; // bottom right corner of a bbox
-    let width = p2.x - p1.x;
-    let height = p2.y - p1.y;
+    let p1 = origin = {x: w / 3, y: h / 3} // upper left corner of a bbox
+    let p2 = opposite = {x: 2 * w / 3, y: 2 * h / 3} // bottom right corner of a bbox
+    let width = p2.x - p1.x
+    let height = p2.y - p1.y
 
-    _update();
+    _update()
 
     // MAIN RETURN
     return {
-      draw,
       getBottomRightCorner,
-      getEditPoint,
       getUpperLeftCorner,
-      hasBbox,
+      getEditPoint,
       saveBbox,
       isInside,
-      move,
+      hasBbox,
+      resize,
       reset,
-      resize
-    };
-
-    function getBottomRightCorner() {
-      return p2;
+      move,
+      draw
     }
 
-    function getUpperLeftCorner() {
-      return p1;
+    function getBottomRightCorner () {
+      return p2
     }
 
-    function hasBbox() {
+    function getUpperLeftCorner () {
+      return p1
+    }
+
+    function hasBbox () {
       if (!prevBbox) {
-        return Math.abs(width) > delta && Math.abs(height) > delta;
+        return Math.abs(width) > delta && Math.abs(height) > delta
       }
 
       if (
@@ -65,13 +65,13 @@ export default function CanvasWindow(options) {
         Math.abs(width - prevBbox.width) >= delta ||
         Math.abs(height - prevBbox.height) >= delta
       ) {
-        return Math.abs(width) > delta && Math.abs(height) > delta;
+        return Math.abs(width) > delta && Math.abs(height) > delta
       }
 
       return false
     }
 
-    function saveBbox() {
+    function saveBbox () {
       prevBbox = {
         x: p1.x,
         y: p1.y,
@@ -80,13 +80,13 @@ export default function CanvasWindow(options) {
       }
     }
 
-    function getEditPoint(point) {
+    function getEditPoint (point) {
       const selectedEPs = editPoints.filter(ep =>
         point.x > ep.x - delta &&
         point.y > ep.y - delta &&
         point.x < ep.x + delta &&
         point.y < ep.y + delta
-      );
+      )
 
       if (selectedEPs.length > 0) {
         selectedEP = selectedEPs[0]
@@ -96,7 +96,7 @@ export default function CanvasWindow(options) {
       }
     }
 
-    function isInside(point) {
+    function isInside (point) {
       return (
         point.x > Math.min(origin.x, opposite.x) &&
         point.x < Math.max(origin.x, opposite.x) &&
@@ -105,134 +105,134 @@ export default function CanvasWindow(options) {
       )
     }
 
-    function resize(o, d) {
+    function resize (o, d) {
       switch (selectedEP.id) {
         case 0:
-          height = -1 * Math.max(minSize, Math.abs(height + d.y));
-          width  = -1 * Math.max(minSize, Math.abs(width + d.x));
-          origin = p2;
-          break;
+          height = -1 * Math.max(minSize, Math.abs(height + d.y))
+          width  = -1 * Math.max(minSize, Math.abs(width + d.x))
+          origin = p2
+          break
         case 1:
-          height = Math.max(minSize, height - d.y);
-          origin = { x: p1.x, y: height <= minSize ? p1.y : p1.y + d.y };
-          break;
+          height = Math.max(minSize, height - d.y)
+          origin = { x: p1.x, y: height <= minSize ? p1.y : p1.y + d.y }
+          break
         case 2:
-          height = -1 * Math.max(minSize, Math.abs(height + d.y));
-          width  = Math.max(minSize, Math.abs(width + d.x));
-          origin = {x: p1.x, y: p2.y};
-          break;
+          height = -1 * Math.max(minSize, Math.abs(height + d.y))
+          width  = Math.max(minSize, Math.abs(width + d.x))
+          origin = {x: p1.x, y: p2.y}
+          break
         case 3:
-          width  = Math.max(minSize, width + d.x);
-          origin = p1;
-          break;
+          width  = Math.max(minSize, width + d.x)
+          origin = p1
+          break
         case 4:
-          height = Math.max(minSize, Math.abs(height + d.y));
-          width  = Math.max(minSize, Math.abs(width + d.x));
-          origin = p1;
-          break;
+          height = Math.max(minSize, Math.abs(height + d.y))
+          width  = Math.max(minSize, Math.abs(width + d.x))
+          origin = p1
+          break
         case 5:
-          height = Math.max(minSize, height + d.y);
-          origin = p1;
-          break;
+          height = Math.max(minSize, height + d.y)
+          origin = p1
+          break
         case 6:
-          height = Math.max(minSize, Math.abs(height + d.y));
-          width  = -1 * Math.max(minSize, Math.abs(width + d.x));
-          origin = {x: p2.x, y: p1.y};
-          break;
+          height = Math.max(minSize, Math.abs(height + d.y))
+          width  = -1 * Math.max(minSize, Math.abs(width + d.x))
+          origin = {x: p2.x, y: p1.y}
+          break
         case 7:
-          width  = Math.max(minSize, width - d.x);
-          origin = { x: width <= minSize ? p1.x : p1.x + d.x, y: p1.y };
-          break;
+          width  = Math.max(minSize, width - d.x)
+          origin = { x: width <= minSize ? p1.x : p1.x + d.x, y: p1.y }
+          break
         default:
-          break;
+          break
       }
 
-      opposite = _getOpposite(origin, width, height);
-      _update();
+      opposite = _getOpposite(origin, width, height)
+      _update()
     }
 
-    function move(d) {
+    function move (d) {
       origin = {
         x: origin.x + d.x,
         y: origin.y + d.y
-      };
-      opposite = _getOpposite(origin, width, height);
-      _update();
+      }
+      opposite = _getOpposite(origin, width, height)
+      _update()
     }
 
-    function draw(o, w, h) {
-      origin = o;
-      height = h;
-      width = w;
-      opposite = _getOpposite(origin, width, height);
-      _update();
+    function draw (o, w, h) {
+      origin = o
+      height = h
+      width = w
+      opposite = _getOpposite(origin, width, height)
+      _update()
     }
 
-    function reset() {
-      width = 0;
-      height = 0;
-      origin = {x: 0, y: 0};
-      opposite = {x: 0, y: 0};
-      _update();
+    function reset () {
+      width = 0
+      height = 0
+      origin = {x: 0, y: 0}
+      opposite = {x: 0, y: 0}
+      _update()
     }
 
-    function _update() {
-      editPoints = [];
+    function _update () {
+      editPoints = []
 
       // draw translucent background
-      ctx.globalAlpha = globalAlpha;
-      ctx.fillStyle = 'black';
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
-      ctx.fillRect(0, 0, canvas.width, canvas.height);
+      ctx.globalAlpha = globalAlpha
+      ctx.fillStyle = 'black'
+      ctx.clearRect(0, 0, canvas.width, canvas.height)
+      ctx.fillRect(0, 0, canvas.width, canvas.height)
 
       // return when no bbox is drawn
-      if (!width || !height) return;
+      if (!width || !height) return
 
       // draw transparent bbox
-      ctx.clearRect(origin.x, origin.y, width, height);
+      ctx.clearRect(origin.x, origin.y, width, height)
 
       // calculate upper left corner to work as a coordenates reference
-      p1 = _getUpperLeftCorner();
-      p2 = _getBottomRightCorner();
+      p1 = _getUpperLeftCorner()
+      p2 = _getBottomRightCorner()
 
       // corners
-      editPoints.push(_createEditPoint({x: p1.x, y: p1.y}, 0, 'nwse-resize'));
-      editPoints.push(_createEditPoint({x: p2.x, y: p1.y}, 2, 'nesw-resize'));
-      editPoints.push(_createEditPoint({x: p2.x, y: p2.y}, 4, 'nwse-resize'));
-      editPoints.push(_createEditPoint({x: p1.x, y: p2.y}, 6, 'nesw-resize'));
+      editPoints.push(_createEditPoint({x: p1.x, y: p1.y}, 0, 'nwse-resize'))
+      editPoints.push(_createEditPoint({x: p2.x, y: p1.y}, 2, 'nesw-resize'))
+      editPoints.push(_createEditPoint({x: p2.x, y: p2.y}, 4, 'nwse-resize'))
+      editPoints.push(_createEditPoint({x: p1.x, y: p2.y}, 6, 'nesw-resize'))
 
       // laterals
       // if (!isMobile.phone) {
-      //   editPoints.push(_createEditPoint({x: p2.x - Math.abs(width) / 2, y: p1.y}, 1, 'ns-resize'));
-      //   editPoints.push(_createEditPoint({x: p2.x, y: p2.y - Math.abs(height) / 2}, 3, 'ew-resize'));
-      //   editPoints.push(_createEditPoint({x: p2.x - Math.abs(width) / 2, y: p2.y}, 5, 'ns-resize'));
-      //   editPoints.push(_createEditPoint({x: p1.x, y: p2.y - Math.abs(height) / 2}, 7, 'ew-resize'));
+      //   editPoints.push(_createEditPoint({x: p2.x - Math.abs(width) / 2, y: p1.y}, 1, 'ns-resize'))
+      //   editPoints.push(_createEditPoint({x: p2.x, y: p2.y - Math.abs(height) / 2}, 3, 'ew-resize'))
+      //   editPoints.push(_createEditPoint({x: p2.x - Math.abs(width) / 2, y: p2.y}, 5, 'ns-resize'))
+      //   editPoints.push(_createEditPoint({x: p1.x, y: p2.y - Math.abs(height) / 2}, 7, 'ew-resize'))
       // }
     }
 
-    function _createEditPoint(origin, id, style) {
-      return EditPoint({ origin, id, style }).draw(ctx);
+    function _createEditPoint (origin, id, style) {
+      return EditPoint({ origin, id, style }).draw(ctx)
     }
 
-    function _getOpposite(o, w, h) {
+    function _getOpposite (o, w, h) {
       return {
         x: Math.round(o.x + w),
         y: Math.round(o.y + h)
-      };
+      }
     }
 
-    function _getUpperLeftCorner() {
+    function _getUpperLeftCorner () {
       return {
         x: Math.round(Math.max(Math.min(origin.x, opposite.x), 0)),
         y: Math.round(Math.max(Math.min(origin.y, opposite.y), 0))
-      };
+      }
     }
 
-    function _getBottomRightCorner() {
+    function _getBottomRightCorner () {
       return {
         x: Math.round(Math.min(Math.max(origin.x, opposite.x), canvas.width)),
         y: Math.round(Math.min(Math.max(origin.y, opposite.y), canvas.height))
-      };
+      }
     }
   }
 }
--- bbox.js
+++ bbox.js
@@ -3,308 +3,294 @@ import Rx from 'rx'
 
 import CanvasWindow from './CanvasWindow'
 
-let instance;
+let instance
 
-const Bbox = (options) => {
-  if (!instance) instance = _Bbox(options);
-
-
-  return instance;
-
-  function _Bbox({canvasContainer, img}) {
-    let container;
-    let subscription;
-
-    let image = img;
-
-    let curPos = {x: 0, y: 0};
-    let ratio = 1;
-
-    let down = 'mousedown'
-    let move = 'mousemove'
-    let up = 'mouseup'
-
-    if ((isMobile.phone || isMobile.tablet)) {
-      down = 'touchstart'
-      move = 'touchmove'
-      up = 'touchend'
-    }
-
-    let imageWidth = img.width
-    let imageHeight = img.height
-    const limitWidth = canvasContainer.offsetWidth
-    const limitHeight = canvasContainer.offsetHeight
-
-    while (imageWidth > limitWidth || imageHeight > limitHeight) {
-      const res = resizeImage(image, limitWidth, limitHeight)
-      imageWidth = res.newImage.width
-      imageHeight = res.newImage.height
-      image = res.newImage
-    }
-
-    console.log(image.width, limitWidth, image.height, limitHeight)
-
-    if (image.width === limitWidth) {
-      ratio = limitWidth / img.width
-    } else if (image.height === limitHeight) {
-      ratio = limitHeight / img.height
-    }
-
-    console.log(ratio)
-
-    // create image canvas
-    const canvasBack = document.createElement('canvas');
-    canvasBack.height = Math.floor(image.height) - 1;
-    canvasBack.width = Math.floor(image.width) - 1;
-
-    // draw image on canvas
-    const ctxBack = canvasBack.getContext('2d');
-    ctxBack.drawImage(image, 0, 0, Math.floor(image.width), Math.floor(image.height));
-
-    // create cropping canvas
-    const canvas = document.createElement('canvas')
-    canvas.width = Math.floor(image.width) - 1;
-    canvas.height = Math.floor(image.height) - 1;
-    canvas.style.position = 'absolute';
-    canvas.style.left = 0;
-    canvas.style.cursor = 'crosshair';
-    canvas.setAttribute('id', 'the_canvas')
-
-    const wrapper = document.createElement('div')
-    wrapper.style.height = canvas.height;
-    wrapper.style.margin = '0 auto';
-    wrapper.style.position = 'relative';
-    wrapper.style.width = canvas.width;
-
-    // draw selection window
-    const cw = CanvasWindow({
-      canvas
-    });
-
-    // attach canvases to DOM
-    wrapper.appendChild(canvasBack);
-    wrapper.appendChild(canvas);
-    canvasContainer.appendChild(wrapper);
-
-    // EVENT LISTENERS
-    let md = Rx.Observable.fromEvent(canvas, down).subscribe(_onMousedown)
-    _styleCursorListener();
-
-    // MAIN RETURN
-    return {
-      canvas,
-      dispose,
-      subscribe
-    };
-
-    function dispose() {
-      // clear DOM
-      canvasContainer.innerHTML = '';
-
-      // cancel event listeners
-      if (md.dispose) md.dispose();
-
-      // clear bbox instance
-      instance = null;
-    }
-
-    function subscribe(callback) {
-      subscription = callback;
-    }
-
-    function _onMousedown(md) {
-      md.preventDefault();
-
-      canvas.style.zIndex = 10;
-
-      // get mouse position relative to container
-      curPos = _getPosition(md, canvas);
-
-      const ep = cw.getEditPoint(curPos)
-      let callback;
-
-      if (ep) {
-        callback = _handleRectResize;
-      } else if (cw.isInside(curPos)) {
-        callback = _handleRectMove;
-      }
-
-      if (callback) {
-        // redraw canvas on mousemoves
-        Rx.Observable
-          .fromEvent(canvasContainer, move)
-          .map(callback)
-          .takeUntil(Rx.Observable.fromEvent(canvasContainer, up))
-          .subscribe(_redrawCanvas)
-
-        // Callback subscription, if there is any, on mouse up
-        Rx.Observable
-          .fromEvent(canvasContainer, up)
-          .take(1) // take only first event of series
-          .subscribe(_onMouseup)
-      }
-    }
-
-    function _onMouseup(e) {
-      curPos = _getPosition(e);
-
-      if (cw.hasBbox() && subscription) {
-        cw.saveBbox()
-
-        const origin = cw.getUpperLeftCorner();
-        const final = cw.getBottomRightCorner();
-
-        subscription({
-          x1: Math.round(origin.x / ratio),
-          x2: Math.round(final.x / ratio),
-          y1: Math.round(origin.y / ratio),
-          y2: Math.round(final.y / ratio)
-        })
-      }
-
-      return _styleCursorListener()
-    }
-
-    function _handleRectResize(e) {
-      const res = _getDelta(curPos, e)
-      curPos = _getPosition(e);
-
-      return {
-        type: 'resize',
-        data: {
-          origin: curPos,
-          delta: res
-        }
-      };
-    }
-
-    function _handleRectMove(e) {
-      const res = _getDelta(curPos, e)
-      curPos = _getPosition(e);
-
-      return {
-        type: 'move',
-        data: res
-      }
-    }
-
-    function _handleRectDraw(e) {
-      const res = _getRectangle(curPos, e)
-      return {
-        type: 'draw',
-        data: res
-      }
-    }
-
-    function _redrawCanvas(e) {
-      switch (e.type) {
-        case 'draw':
-          cw.draw(e.data.origin, e.data.width, e.data.height);
-          break;
-        case 'move':
-          cw.move(e.data);
-          break;
-        case 'resize':
-          cw.resize(e.data.origin, e.data.delta);
-          break;
-        default:
-          break;
-      }
-    }
-
-    function _styleCursor(e) {
-      const pos = _getPosition(e, canvasContainer);
-      const ep = cw.getEditPoint(pos);
-
-      if (ep) {
-        canvas.style.cursor = ep.style;
-      } else if (cw.isInside(pos) && canvas.style.cursor !== 'move') {
-        canvas.style.cursor = 'move';
-      } else if (!cw.isInside(pos) && canvas.style.cursor !== 'default') {
-        canvas.style.cursor = 'default';
-      }
-    }
-
-    function resizeImage(image, limitWidth, limitHeight) {
-      if (
-        typeof image === 'undefined' ||
-        typeof limitWidth === 'undefined' ||
-        typeof limitHeight === 'undefined'
-      ) {
-        throw new Error('missing argument')
-      }
-
-      if (image.width > limitWidth) {
-        return resizeWidth(image, limitWidth)
-      } else if (image.height > limitHeight) {
-        return resizeHeight(image, limitHeight)
-      }
-    }
-
-    function resizeWidth(image, limitWidth) {
-      const newImage = document.createElement('img');
-      const ratio = limitWidth / image.width;
-
-      newImage.src = image.src;
-      newImage.width = image.width * ratio;
-      newImage.height = image.height * ratio;
-
-      return {
-        newImage, ratio
-      };
-    }
-
-    function resizeHeight(image, limitHeight) {
-      const newImage = document.createElement('img');
-      const ratio = limitHeight / image.height;
-
-      newImage.src = image.src;
-      newImage.width = image.width * ratio;
-      newImage.height = image.height * ratio;
-
-      return {
-        newImage, ratio
-      };
-    }
-
-    function _getRectangle(origin, e) {
-      const delta = _getDelta(origin, e);
-      return {
-        origin,
-        height: Math.round(delta.y),
-        width: Math.round(delta.x),
-      };
-    }
-
-    function _getPosition(e) {
-      // get container position on document, it has a performance impact when window is resized
-      container = canvas.getBoundingClientRect();
-
-      if ((isMobile.phone || isMobile.tablet) && !e.touches.length) return
-
-      return {
-        x: Math.round((e.clientX || e.touches[0].clientX) - container.left),
-        y: Math.round((e.clientY || e.touches[0].clientY) - container.top)
-      }
-    }
-
-    function _getDelta(origin, e) {
-      // get container position on document, it has a performance impact when window is resized
-      container = canvas.getBoundingClientRect();
-
-      return {
-        x: Math.round((e.clientX || e.touches[0].clientX) - container.left - origin.x),
-        y: Math.round((e.clientY || e.touches[0].clientY) - container.top - origin.y)
-      }
-    }
-
-    function _styleCursorListener() {
-      return Rx.Observable
-        .fromEvent(canvasContainer, move)
-        .takeUntil(Rx.Observable.fromEvent(canvasContainer, down))
-        .subscribe(_styleCursor);
-    }
-  }
+module.exports = (options) => {
+  if (!instance) instance = BBOX(options)
+  return instance
 }
 
-module.exports = Bbox
+function BBOX ({canvasContainer, img}) {
+  if (img == null || canvasContainer == null) {
+    console.warn(
+      'BBOX: Missing some arguments in here...',
+      `canvasContainer: ${typeof canvasContainer}`,
+      `img: ${typeof img}`
+    )
+    return undefined
+  }
+
+  let container
+  let subscription
+
+  let image = img
+
+  let curPos = {x: 0, y: 0}
+  let ratio = 1
+
+  let down = 'mousedown'
+  let move = 'mousemove'
+  let up = 'mouseup'
+
+  if ((isMobile.phone || isMobile.tablet)) {
+    down = 'touchstart'
+    move = 'touchmove'
+    up = 'touchend'
+  }
+
+  let imageWidth = img.width
+  let imageHeight = img.height
+  const limitWidth = canvasContainer.offsetWidth - canvasContainer.styles.paddingLeft - canvasContainer.styles.paddingRight
+  const limitHeight = canvasContainer.offsetHeight - canvasContainer.styles.paddingBottom - canvasContainer.styles.paddingTop
+
+  while (imageWidth > limitWidth || imageHeight > limitHeight) {
+    const res = resizeImage(image, limitWidth, limitHeight)
+    imageWidth = res.newImage.width
+    imageHeight = res.newImage.height
+    image = res.newImage
+  }
+
+  if (image.width === limitWidth) {
+    ratio = limitWidth / img.width
+  } else if (image.height === limitHeight) {
+    ratio = limitHeight / img.height
+  }
+
+  // create image canvas
+  const canvasBack = document.createElement('canvas')
+  canvasBack.height = Math.floor(image.height) - 1
+  canvasBack.width = Math.floor(image.width) - 1
+
+  // draw image on canvas
+  const ctxBack = canvasBack.getContext('2d')
+  ctxBack.drawImage(image, 0, 0, Math.floor(image.width), Math.floor(image.height))
+
+  // create cropping canvas
+  const canvas = document.createElement('canvas')
+  canvas.width = Math.floor(image.width) - 1
+  canvas.height = Math.floor(image.height) - 1
+  canvas.style.position = 'absolute'
+  canvas.style.left = 0
+  canvas.style.cursor = 'crosshair'
+  canvas.setAttribute('id', 'the_canvas')
+
+  const wrapper = document.createElement('div')
+  wrapper.style.height = canvas.height
+  wrapper.style.margin = '0 auto'
+  wrapper.style.position = 'relative'
+  wrapper.style.width = canvas.width
+
+  // draw selection window
+  const cw = CanvasWindow({
+    canvas
+  })
+
+  // attach canvases to DOM
+  wrapper.appendChild(canvasBack)
+  wrapper.appendChild(canvas)
+  canvasContainer.appendChild(wrapper)
+
+  // EVENT LISTENERS
+  let md = Rx.Observable.fromEvent(canvas, down).subscribe(onMousedown)
+  styleCursorListener()
+
+  // MAIN RETURN
+  return {
+    canvas,
+    dispose,
+    subscribe
+  }
+
+  function dispose () {
+    // clear DOM
+    canvasContainer.innerHTML = ''
+
+    // cancel event listeners
+    if (md.dispose) md.dispose()
+
+    ratio = 1
+
+    // clear BBOX instance
+    instance = null
+  }
+
+  function subscribe (callback) {
+    subscription = callback
+  }
+
+  function onMousedown (md) {
+    md.preventDefault()
+
+    canvas.style.zIndex = 10
+
+    // get mouse position relative to container
+    curPos = _getPosition(md, canvas)
+
+    const ep = cw.getEditPoint(curPos)
+    let callback
+
+    if (ep) {
+      callback = _handleRectResize
+    } else if (cw.isInside(curPos)) {
+      callback = _handleRectMove
+    }
+
+    if (callback) {
+      // redraw canvas on mousemoves
+      Rx.Observable
+        .fromEvent(canvasContainer, move)
+        .map(callback)
+        .takeUntil(Rx.Observable.fromEvent(canvasContainer, up))
+        .subscribe(redrawCanvas)
+
+      // Callback subscription, if there is any, on mouse up
+      Rx.Observable
+        .fromEvent(canvasContainer, up)
+        .take(1) // take only first event of series
+        .subscribe(_onMouseup)
+    }
+  }
+
+  function _onMouseup (e) {
+    curPos = _getPosition(e)
+
+    if (cw.hasBBOX() && subscription) {
+      cw.saveBBOX()
+
+      const origin = cw.getUpperLeftCorner()
+      const final = cw.getBottomRightCorner()
+
+      subscription({
+        x1: Math.round(origin.x / ratio),
+        x2: Math.round(final.x / ratio),
+        y1: Math.round(origin.y / ratio),
+        y2: Math.round(final.y / ratio)
+      })
+    }
+
+    return styleCursorListener()
+  }
+
+  function _handleRectResize (e) {
+    const res = _getDelta(curPos, e)
+    curPos = _getPosition(e)
+
+    return {
+      type: 'resize',
+      data: {
+        origin: curPos,
+        delta: res
+      }
+    }
+  }
+
+  function _handleRectMove (e) {
+    const res = _getDelta(curPos, e)
+    curPos = _getPosition(e)
+
+    return {
+      type: 'move',
+      data: res
+    }
+  }
+
+  function redrawCanvas (e) {
+    switch (e.type) {
+      case 'draw':
+        cw.draw(e.data.origin, e.data.width, e.data.height)
+        break
+      case 'move':
+        cw.move(e.data)
+        break
+      case 'resize':
+        cw.resize(e.data.origin, e.data.delta)
+        break
+      default:
+        break
+    }
+  }
+
+  function _styleCursor (e) {
+    const pos = _getPosition(e, canvasContainer)
+    const ep = cw.getEditPoint(pos)
+
+    if (ep) {
+      canvas.style.cursor = ep.style
+    } else if (cw.isInside(pos) && canvas.style.cursor !== 'move') {
+      canvas.style.cursor = 'move'
+    } else if (!cw.isInside(pos) && canvas.style.cursor !== 'default') {
+      canvas.style.cursor = 'default'
+    }
+  }
+
+  function resizeImage (image, limitWidth, limitHeight) {
+    if (
+      typeof image === 'undefined' ||
+      typeof limitWidth === 'undefined' ||
+      typeof limitHeight === 'undefined'
+    ) {
+      throw new Error('missing argument')
+    }
+
+    if (image.width > limitWidth) {
+      return resizeWidth(image, limitWidth)
+    } else if (image.height > limitHeight) {
+      return resizeHeight(image, limitHeight)
+    }
+  }
+
+  function resizeWidth (image, limitWidth) {
+    const newImage = document.createElement('img')
+    const ratio = limitWidth / image.width
+
+    newImage.src = image.src
+    newImage.width = image.width * ratio
+    newImage.height = image.height * ratio
+
+    return {
+      newImage, ratio
+    }
+  }
+
+  function resizeHeight (image, limitHeight) {
+    const newImage = document.createElement('img')
+    const ratio = limitHeight / image.height
+
+    newImage.src = image.src
+    newImage.width = image.width * ratio
+    newImage.height = image.height * ratio
+
+    return {
+      newImage, ratio
+    }
+  }
+
+  function _getPosition (e) {
+    // get container position on document, it has a performance impact when window is resized
+    container = canvas.getBoundingClientRect()
+
+    if ((isMobile.phone || isMobile.tablet) && !e.touches.length) return
+
+    return {
+      x: Math.round((e.clientX || e.touches[0].clientX) - container.left),
+      y: Math.round((e.clientY || e.touches[0].clientY) - container.top)
+    }
+  }
+
+  function _getDelta (origin, e) {
+    // get container position on document, it has a performance impact when window is resized
+    container = canvas.getBoundingClientRect()
+
+    return {
+      x: Math.round((e.clientX || e.touches[0].clientX) - container.left - origin.x),
+      y: Math.round((e.clientY || e.touches[0].clientY) - container.top - origin.y)
+    }
+  }
+
+  function styleCursorListener () {
+    return Rx.Observable
+      .fromEvent(canvasContainer, move)
+      .takeUntil(Rx.Observable.fromEvent(canvasContainer, down))
+      .subscribe(_styleCursor)
+  }
+}
